6f08df306b43c1c3dd6c6322fc0382647db4de67
Saeedo1998 <the_gamer_98@hotmail.com>
2021-10-29 19:47:59 +0300

    Added some JavaScript

---
+++ Script.js
@@ -0,0 +1,30 @@
+//     scrapped idea 
+//     code for converting overall website style from mutiple pages to one page with panel navigation (scrapped) 
+//     was in Index.html
+//     https://stackoverflow.com/questions/19463651/how-to-change-the-class-of-an-html-button-at-runtime-on-its-click-event 
+function activeHome() {
+    //    $(document.getElementById('HomeLink')).toggleClass('active');
+    $(document.getElementById('HomeLink')).addClass('active');
+    //    document.getElementById('Hide').style.display='block';
+    // document.getElementById('Show').style.display='none';
+};
+
+function changePageTitle() {
+    // newPageTitle = 'Saeed Badghaish';
+    document.title = 'Saeed Badghaish';
+}
+
+// function redirect() {
+//     if (window.location.href = "~/Pages/Portfolio.html") {
+//         window.location.href = "https://www.saeedo98.com/"
+//     }
+// }
+
+//global call (automatically called in all pages)
+window.onload = function () {
+    var url = location.href;
+    if (url = "~/Pages/Portfolio.html") {
+        location.href = "../Index.html"
+    }
+}
+
