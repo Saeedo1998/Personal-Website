c93f56b8dd20ed655c1d1b1c08f4dac19e6f89b4
Saeedo1998 <the_gamer_98@hotmail.com>
2021-11-27 22:25:55 +0300

    Audio

--- Script.js
+++ Script.js
@@ -3,29 +3,29 @@
 //     was in Index.html
 //     https://stackoverflow.com/questions/19463651/how-to-change-the-class-of-an-html-button-at-runtime-on-its-click-event 
 function activeHome() {
-    //    $(document.getElementById('HomeLink')).toggleClass('active');
-    $(document.getElementById('HomeLink')).addClass('active');
-    //    document.getElementById('Hide').style.display='block';
-    // document.getElementById('Show').style.display='none';
+  //    $(document.getElementById('HomeLink')).toggleClass('active');
+  $(document.getElementById('HomeLink')).addClass('active');
+  //    document.getElementById('Hide').style.display='block';
+  // document.getElementById('Show').style.display='none';
 };
 
 window.onload = function changePageTitle() {
-    // newPageTitle = 'Saeed Badghaish';
-    var url = window.location.href;
-    if (url.includes("Portfolio")) {
-        document.title = 'Saeed Badghaish - Portfolio';
-    } else if (url.includes("css-playground")){
-        document.title = 'Saeed Badghaish - CSS Playground';
-    }
-    else {
-        document.title = 'Saeed Badghaish - Home';
-    }
+  // newPageTitle = 'Saeed Badghaish';
+  var url = window.location.href;
+  if (url.includes("Portfolio")) {
+    document.title = 'Saeed Badghaish - Portfolio';
+  } else if (url.includes("css-playground")) {
+    document.title = 'Saeed Badghaish - CSS Playground';
+  }
+  else {
+    document.title = 'Saeed Badghaish - Home';
+  }
 
 }
 
 function redirectToHome() {
-    document.writeln("");
-    window.location.href = "https://www.saeedo98.com/"
+  document.writeln("");
+  window.location.href = "https://www.saeedo98.com/"
 }
 
 //global call (automatically called in all pages)
@@ -37,47 +37,87 @@ function redirectToHome() {
 // }
 
 function copyTextOld(text) {
-    // var copyText = document.getElementById("myInput");
-    var copyText = text;
-    copyText.select();
-    copyText.setSelectionRange(0, 99999);
-    navigator.clipboard.writeText(copyText.value);
-    
-    var tooltip = document.getElementById("myTooltip");
-    // tooltip.innerHTML = "Copied: " + copyText.value;
-    tooltip.innerHTML = "Copied Successfully";
+  // var copyText = document.getElementById("myInput");
+  var copyText = text;
+  copyText.select();
+  copyText.setSelectionRange(0, 99999);
+  navigator.clipboard.writeText(copyText.value);
 
+  var tooltip = document.getElementById("myTooltip");
+  // tooltip.innerHTML = "Copied: " + copyText.value;
+  tooltip.innerHTML = "Copied Successfully";
+
+}
+
+function copyTextFromElement(elementID, actionElementID) {
+  let element = document.getElementById(elementID); //select the element
+  let elementText = element.textContent; //get the text content from the element
+  copyText(elementText); //use the copyText function below
+  let actionElement = document.getElementById(actionElementID);
+  actionElement.innerHTML = "Copied!";
+}
+
+function copyText(text) {
+  navigator.clipboard.writeText(text);
+}
+
+function mouseLeave(elementID) {
+  let element = document.getElementById(elementID); //select the element
+  element.innerHTML = "Copy";
+}
+
+async function playAnimRotate(element) {
+  document.getElementById(element).classList.add('rotate');
+  await delay(1100);
+  document.getElementById(element).classList.remove('rotate');
+
+}
+
+function delay(time) {
+  return new Promise(resolve => setTimeout(resolve, time));
+}
+
+// delay(1000).then(() => console.log('ran after 1 second1 passed'));
+
+function playEasterEggSong() {
+    // const audio = new Audio("Portal/12StillAlive.mp3");
+    const audio = document.getElementById('audioPortal');
+    audio.play();
+      
   }
 
-  function copyTextFromElement(elementID, actionElementID) {
-    let element = document.getElementById(elementID); //select the element
-    let elementText = element.textContent; //get the text content from the element
-    copyText(elementText); //use the copyText function below
-    let actionElement = document.getElementById(actionElementID);
-    actionElement.innerHTML = "Copied!";
-  }
+  // let isAudioPlaying = false;
+  // let audioText = document.getElementById('audioText');
+// function playEasterEggSong() {
+//   // const audio = new Audio("Portal/12StillAlive.mp3");
+//   const audio = document.getElementById('audioPortal');
+//   if (isAudioPlaying == true) {
+//     //do nothing
+//     audioText.textContent = "Not Playing";
+//   } else if (isAudioPlaying == false) {
+//     audio.play();
+//     audioText.textContent = "Playing";
+//   } else {
+//   }
+//   audio.addEventListener("canplaythrough", event => {
+//     /* the audio is now playable; play it if permissions allow */
 
-  function copyText(text) {
-    navigator.clipboard.writeText(text);
-  }
+//   });
+//   // document.querySelector('turret').disabled = true;
+// }
 
-  function mouseLeave(elementID) {
-    let element = document.getElementById(elementID); //select the element
-    element.innerHTML = "Copy";
-  }
+// function stopPlayback() {
+//   if (isAudioPlaying == false) {
+//     isAudioPlaying = true;
 
-  async function playAnimRotate(element) {
-    document.getElementById(element).classList.add('rotate');
-    await delay(1100);
-    document.getElementById(element).classList.remove('rotate');
+//   }
 
-  }
+// }
 
-  function delay(time) {
-    return new Promise(resolve => setTimeout(resolve, time));
-  }
-  
-  // delay(1000).then(() => console.log('ran after 1 second1 passed'));
-  
+// function allowPlayback() {
+//   if (isAudioPlaying == true) {
+//     isAudioPlaying = false;
 
+//   }
+// }
 
