6550b06e30a7135a1664f94c6a8d3b9c76b670c4
Saeedo1998 <Saeedo_98@hotmail.com>
2021-12-04 23:45:46 +0300

    Added web component for footer

---
+++ Footer.js
@@ -0,0 +1,20 @@
+
+// window.onload = function loadFooter() {
+//     document.write('\
+//     <div class="footer text-center"> \
+//         Saeed Badghaish © 2021 \
+//     </div> \
+//     ')
+// }
+
+//YESSS FUCKING FINALLY WORKED
+class Footer extends HTMLElement {
+    connectedCallback() {
+      this.innerHTML = `<div class="footer text-center">
+      Saeed Badghaish © 2021
+      </div>
+      `;
+    }
+  }
+      
+  customElements.define('footer-saeed', Footer);
