449903e38beb9b99fa5f8fcd9edfe90c2f0f8a6c
Saeedo1998 <the_gamer_98@hotmail.com>
2022-08-05 23:17:38 +0300

    Removed icon system implementation due to non functionality

--- Icon.js
+++ Icon.js
@@ -1,3 +1,4 @@
+// Does not work
 class Icon extends HTMLElement {
     connectedCallback() {
       this.innerHTML = `<link rel="icon" href="Images\\S-logo-inverted-circle-blue-1024x1024-circle2.png">`;
