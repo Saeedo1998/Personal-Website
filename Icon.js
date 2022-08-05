0df40accfdef96a33617138c47fb04331ceda178
Saeedo1998 <the_gamer_98@hotmail.com>
2022-08-05 23:15:33 +0300

    Fix for icon system

--- Icon.js
+++ Icon.js
@@ -1,6 +1,6 @@
 class Icon extends HTMLElement {
     connectedCallback() {
-      this.innerHTML = `<link rel="icon" href="Images\S-logo-inverted-circle-blue-1024x1024-circle2.png">`;
+      this.innerHTML = `<link rel="icon" href="Images\\S-logo-inverted-circle-blue-1024x1024-circle2.png">`;
     }
   }
       
